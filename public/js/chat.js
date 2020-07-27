429210a68c4539b9ef1c81b5a9492fc971e2e9ba
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-27 22:10:15 +0530

    Sending and receiving data between a client and server

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io()
+'use strict'
+
+const socket = io();
+
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated!', count);
+});
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('clicked');
+    socket.emit('increment');
+});
