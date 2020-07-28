92153204b429d79ea7d37bf6cb01d1c5c8a8f032
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-28 14:42:52 +0530

    Sending messages using Socket.io events

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,11 +2,14 @@
 
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count);
+socket.on('message', (message) => {
+    console.log(message);
 });
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('clicked');
-    socket.emit('increment');
+document.querySelector('#messageForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const message = document.querySelector('input').value;
+
+    socket.emit('sendMessage', message);
 });
