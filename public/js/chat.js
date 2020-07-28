7ae5a47ada2c5b1a2907b308d2c0bb6235e11875
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-28 15:55:57 +0530

    Sending location to other users

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,3 +13,18 @@ document.querySelector('#messageForm').addEventListener('submit', (event) => {
 
     socket.emit('sendMessage', message);
 });
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your Browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const location = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+
+        socket.emit('sendLocation', location);
+    });
+});
